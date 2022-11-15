1a2a76b9cdc3643850672aa8cb9e560a66644e7f
Sebastián López Osorno <sebastianlopezosorno2005@gmail.com>
2022-11-14 21:21:29 -0500

    main

--- boostrap&jquery/main.js
+++ boostrap&jquery/main.js
@@ -1,71 +1,61 @@
-$(document).ready(function () {
+$(function () {
+    $("#submit").click(function (elem) {
+        elem.preventDefault();
+        if ($("#password1").val() == $("#password2").val() && $("#password1").val() != "") {
 
-    $("#registro").validate({
-        rules: {
-            email1:"required",
-            email2:"required",
-            password1:"required",
-            password2:"required",
-            email1: {
-                required: true,
-                email: true,
-                depends: function (elem) {
-                    return $("#email1").val() == $("#email2").val()
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                    toast.addEventListener('mouseenter', Swal.stopTimer)
+                    toast.addEventListener('mouseleave', Swal.resumeTimer)
                 }
-            },
-            email2: {
-                required: true,
-                email: true,
-                depends: function (elem) {
-                    return $("#email1").val() == $("#email2").val()
+            })
+
+            Toast.fire({
+                icon: 'success',
+                title: 'Sesion iniciada'
+            })
+
+            setTimeout(function () {
+                window.location.href = "index.html"
+            }, 4000);
+
+
+        } else {
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                    toast.addEventListener('mouseenter', Swal.stopTimer)
+                    toast.addEventListener('mouseleave', Swal.resumeTimer)
                 }
-            },
-            password1: {
-                required: true,
-                minlength: 8,
-                depends: function (elem) {
-                    return $("#password1").val() == $("#password2").val()
-                },
-                passwordcheck:true
-            },
-            password2: {
-                required: true,
-                minlength: 8,
-                depends: function (elem) {
-                    return $("#password1").val() == $("#password2").val()
-                },
-                passwordcheck:true
-            }
-        },
-        messages: {
-            email1: {
-                required: "Por favor, introduzca su Correo.",
-                email: "Correo no valido",
-                depends: "Los correos no coinciden"
-            },
-            email2: {
-                required: "Por favor, introduzca su Correo.",
-                email: "Correo no valido",
-                depends: "Los correos no coinciden"
-            },
-            password1: {
-                required: "Por favor proporcione una contraseña",
-                minlength: "Su contraseña debe tener al menos 5 caracteres.",
-                depends: "Las contraseñas no coinciden"
-            },
-            password2: {
-                required: "Por favor proporcione una contraseña",
-                minlength: "Su contraseña debe tener al menos 5 caracteres.",
-                depends: "Las contraseñas no coinciden"
-            },
-            email1: "Por favor, introduce una dirección de correo electrónico válida",
-            email2: "Por favor, introduce una dirección de correo electrónico válida"
-        },
-        submitHandler: function (form) {
-            if($("#password1").val() == $("#password2").val()){
-                form.submit();
-            }
+            })
+
+            Toast.fire({
+                icon: 'error',
+                title: 'No se ha podido iniciar sesion'
+            })
         }
+    })
+    $("#append").click(function () {
+        $("#contenedor").append('\
+            <div class="col-md-4 d-flex justify-content-center align-items-center mb-5"> \
+                <div class="card" style="width: 18rem; background-color: #202020;"> \
+                    <img src="https://img.icons8.com/color/480/person-male.png" class="card-img-top" alt = "..."> \
+                    <div class="card-body"> \
+                        <h5 class="card-title">'+$("#name").val()+'</h5> \
+                        <p class="card-text">'+$("#descripction").val()+'.</p> \
+                        <a href="#" class="btn btn-primary">Ver</a> \
+                    </div> \
+                </div > \
+            </div> \
+        ');
     });
-});
-
+})
