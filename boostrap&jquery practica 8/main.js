4cc9fc2e2d7b7a76a26b5fa2fb69d04ccb994cb1
Sebastián López Osorno <100486485+SebastianLopezO@users.noreply.github.com>
2022-11-15 09:13:18 -0500

    main

--- boostrap&jquery practica 8/main.js
+++ boostrap&jquery practica 8/main.js
@@ -1,32 +1,35 @@
 $(function () {
     $("#append").click(function () {
-        if ($("#name").val() != "" && $("#description").val() != "") {
-        
-            const Toast = Swal.mixin({
-                toast: true,
-                position: 'top-end',
-                showConfirmButton: false,
-                timer: 3000,
-                timerProgressBar: true,
-                didOpen: (toast) => {
-                    toast.addEventListener('mouseenter', Swal.stopTimer)
-                    toast.addEventListener('mouseleave', Swal.resumeTimer)
-                }
-            })
+        if ($("#name").val() != "" && $("#email").val() != "") {
+            var paramsemail = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+            var paramsname= /^[a-zA-Z ]+(,[a-zA-Z ]+)*$/;
+            if (paramsemail.test($("#name").val()) && paramsname.test($("#email").val())) {
+                const Toast = Swal.mixin({
+                    toast: true,
+                    position: 'top-end',
+                    showConfirmButton: false,
+                    timer: 3000,
+                    timerProgressBar: true,
+                    didOpen: (toast) => {
+                        toast.addEventListener('mouseenter', Swal.stopTimer)
+                        toast.addEventListener('mouseleave', Swal.resumeTimer)
+                    }
+                })
 
-            Toast.fire({
-                icon: 'success',
-                title: 'Se ha agregado correctamente'
-            })
-        
-            setTimeout(function () {
-                $("#contenedor").append('\
+                Toast.fire({
+                    icon: 'success',
+                    title: 'Se ha agregado correctamente'
+                })
+
+                setTimeout(function () {
+                    $("#contenido").append('\
                     <tr> \
-                        <td>'+$("#name").val()+'</td>\
-                        <td>'+$("#email").val()+'</td>\
+                        <td>'+ $("#name").val() + '</td>\
+                        <td>'+ $("#email").val() + '</td>\
                         <td><button type="button" class="btn btn-warning">Modificar</button><button type="button" class="btn btn-danger">Eliminar</button></td> \
                     </tr> ');
-                }, 4000);   
+                }, 4000);
+            }
         } else {
             const Toast = Swal.mixin({
                 toast: true,
