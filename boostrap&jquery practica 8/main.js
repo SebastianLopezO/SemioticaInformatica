9ae2674890781afb258470cd7b59c29e21bf5aff
Sebastián López Osorno <100486485+SebastianLopezO@users.noreply.github.com>
2022-11-15 09:20:53 -0500

    main

--- boostrap&jquery practica 8/main.js
+++ boostrap&jquery practica 8/main.js
@@ -2,7 +2,7 @@ $(function () {
     $("#append").click(function () {
         if ($("#name").val() != "" && $("#email").val() != "") {
             var paramsemail = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
-            var paramsname= /^[a-zA-Z ]+(,[a-zA-Z ]+)*$/;
+            var paramsname = /^[a-zA-Z ]+(,[a-zA-Z ]+)*$/;
             if (paramsemail.test($("#name").val()) && paramsname.test($("#email").val())) {
                 const Toast = Swal.mixin({
                     toast: true,
@@ -29,6 +29,24 @@ $(function () {
                         <td><button type="button" class="btn btn-warning">Modificar</button><button type="button" class="btn btn-danger">Eliminar</button></td> \
                     </tr> ');
                 }, 4000);
+            } else {
+
+                const Toast = Swal.mixin({
+                    toast: true,
+                    position: 'top-end',
+                    showConfirmButton: false,
+                    timer: 3000,
+                    timerProgressBar: true,
+                    didOpen: (toast) => {
+                        toast.addEventListener('mouseenter', Swal.stopTimer)
+                        toast.addEventListener('mouseleave', Swal.resumeTimer)
+                    }
+                })
+
+                Toast.fire({
+                    icon: 'warning',
+                    title: 'No es un correo valido'
+                })
             }
         } else {
             const Toast = Swal.mixin({
