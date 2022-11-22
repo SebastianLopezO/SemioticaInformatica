ebe5bc9787a48e369d0922ee4b31c0eb7196d310
Sebastián López Osorno <sebastianlopezosorno2005@gmail.com>
2022-11-22 13:41:02 -0500

    main

--- boostrap&jquery practica 9/js/main.js
+++ boostrap&jquery practica 9/js/main.js
@@ -2,14 +2,17 @@ $(function () {
     $("#generate").click(function () {
         var age=Math.round(Math.random()*(30-10)+10)
         var id=Math.round(Math.random()*(10-1)+1)
+        var sex=Math.round(Math.random()*(2-1)+1)
         $("#age").val(age)
         $("#name").val("Usuario"+id)
+        sex==1?$("#sex").val("masculino"):$("#sex").val("femenino")
     });
 
     $("#send").click(function () {
-        if($("#name").val()!="" && $("#age").val()!=""){
+        if($("#name").val()!="" && $("#age").val()!="" && $("#sex").val()!=""){
             sessionStorage.setItem("name", $("#name").val())
             sessionStorage.setItem("age", $("#age").val())
+            sessionStorage.setItem("sex", $("#sex").val())
             alerta('success', 'Los datos han sido almacenados, redirigiendo');
             
             setTimeout(() => {  window.location.href = "usuario.html"; }, 4000);
@@ -19,15 +22,23 @@ $(function () {
     });
 
     var img=""
-    sessionStorage.age>=18?img="/img/user2.png":img="/img/user1.png"
+    var type=""
+    var sex=""
+    if(sessionStorage.age>=18){
+        sessionStorage.sex=="masculino"?img="img/man.png":img="img/woman.png"
+        type ="Mayor"
+    }else{
+        sessionStorage.sex=="masculino"?img="img/boy.png":img="img/girl.png"
+        type ="Menor"
+    }
 
     $("#contenedor").append('\
-    <div class="col-md-4 d-flex justify-content-center align-items-center mb-5"> \
+    <div class="col d-flex justify-content-center align-items-center mb-5"> \
         <div class="card" style="width: 18rem; background-color: #202020;"> \
             <img src="'+img+'" class="card-img-top" alt = "..."> \
             <div class="card-body"> \
                 <h5 class="card-title">'+ sessionStorage.name + '</h5> \
-                <p class="card-text">'+ sessionStorage.age + '</p> \
+                <p class="card-text">El usuario con el nombre '+ sessionStorage.name +" con sexo "+sessionStorage.sex+" tiene "+ sessionStorage.age +" años, por lo cual es "+type+ '</p> \
                 <a href="#" class="btn btn-primary">Ver</a> \
             </div> \
         </div > \
