9089468c6a5136595b262cc5e3e210b26bd882ff
Sebastián López Osorno <100486485+SebastianLopezO@users.noreply.github.com>
2022-11-22 08:47:55 -0500

    main

--- boostrap&jquery practica 9/js/main.js
+++ boostrap&jquery practica 9/js/main.js
@@ -1,19 +1,9 @@
-function check(e) {
-    tecla = (document.all) ? e.keyCode : e.which;
-
-    //Tecla de retroceso para borrar, siempre la permite
-    if (tecla == 8) {
-        return true;
-    }
-    patron = /^[a-zA-Z ]+(,[a-zA-Z ]+)*$/;
-    tecla_final = String.fromCharCode(tecla);
-    return patron.test(tecla_final);
-}
-
 $(function () {
     $("#scooch").click(function () {
-        var age=Math.round(Math.random()*(99-1)+1)
-        $("#scooch").val(age)
+        var age=Math.round(Math.random()*(30-10)+10)
+        var id=Math.round(Math.random()*(10-1)+1)
+        $("#age").val(age)
+        $("#name").val("Usuario"+id)
     });
 
 });
