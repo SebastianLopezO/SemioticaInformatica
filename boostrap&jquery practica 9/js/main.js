62ec2016985baa93df014684367fa232d3e075ec
Sebastián López Osorno <100486485+SebastianLopezO@users.noreply.github.com>
2022-11-22 09:46:54 -0500

    main

--- boostrap&jquery practica 9/js/main.js
+++ boostrap&jquery practica 9/js/main.js
@@ -10,11 +10,32 @@ $(function () {
         if($("#name").val()!="" && $("#age").val()!=""){
             sessionStorage.setItem("name", $("#name").val())
             sessionStorage.setItem("age", $("#age").val())
-            window.location.href = "usuario.html";
-        }else{
+            alerta('success', 'Los datos han sido almacenados, redirigiendo');
             
+            setTimeout(() => {  window.location.href = "usuario.html"; }, 4000);
+        }else{
+            alerta('error', 'Los datos son incorrectos');
         }
     });
 
     $("#contenedor").append("<h1>test</h1>"+sessionStorage.name+sessionStorage.age);
+
+    function alerta(icon, title) {
+        const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+                toast.addEventListener('mouseenter', Swal.stopTimer)
+                toast.addEventListener('mouseleave', Swal.resumeTimer)
+            }
+        })
+
+        Toast.fire({
+            icon: icon,
+            title: title
+        })
+    }
 });
