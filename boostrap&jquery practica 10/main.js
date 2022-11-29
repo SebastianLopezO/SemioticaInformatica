163d50d399f576c8fb0005d3da8caf4e1535e418
Sebastián López Osorno <100486485+SebastianLopezO@users.noreply.github.com>
2022-11-29 08:18:15 -0500

    main

--- boostrap&jquery practica 10/main.js
+++ boostrap&jquery practica 10/main.js
@@ -1,8 +1,36 @@
 $(function () {
-
+    var nums=[]
     $("#generate").click(function () {
-        var numero= Math.round(Math.random()*(10-1)+1);
-        $("#number").val(numero);
+        let max=$("#max").val();
+        let min=$("#min").val();
+        do{
+            var num=Math.round(Math.random()*(max-min)+min)
+            if(nums.indexOf()<0 || nums.length==0){
+                nums.push();
+                break;
+            }else if(nums.length()==max){
+                alerta("error","La lista ya esta llena");
+            }
+        }while(true);
     });
+
+    function alerta(icon, title) {
+        const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+                toast.addEventListener('mouseenter', Swal.stopTimer)
+                toast.addEventListener('mouseleave', Swal.resumeTimer)
+            }
+        })
+
+        Toast.fire({
+            icon: icon,
+            title: title
+        })
+    }
 
 });
