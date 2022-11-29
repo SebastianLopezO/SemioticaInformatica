609516b921c2636db7fa3db197f2b5c3fd888281
Sebastián López Osorno <100486485+SebastianLopezO@users.noreply.github.com>
2022-11-29 08:31:41 -0500

    main

--- boostrap&jquery practica 10/main.js
+++ boostrap&jquery practica 10/main.js
@@ -5,7 +5,7 @@ $(function () {
         let min=parseInt($("#min").val());
         do{
             var num=Math.round(Math.random()*(max-min)+min)
-            if(nums.indexOf(num)<0 || nums.length==0){
+            if(nums.indexOf(num)<0 || nums.length()==0){
                 nums.push(num);
                 break;
             }else if(nums.length()==max){
