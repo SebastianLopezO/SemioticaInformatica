3b9046f3d2e882f12dd938158063cdd6d58871ef
Sebastián López Osorno <100486485+SebastianLopezO@users.noreply.github.com>
2022-12-13 09:26:55 -0500

    main

--- boostrap&jquery final/main.js
+++ boostrap&jquery final/main.js
@@ -66,4 +66,35 @@ $(function () {
             }while(true);
             $("#contenido").append('');
     }
+
+    function alerta(icon, title) {
+        const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+                toast.addEventListener('mouseenter', Swal.stopTimer)
+                toast.addEventListener('mouseleave', Swal.resumeTimer)
+            }
+        })
+
+        Toast.fire({
+            icon: icon,
+            title: title
+        })
+    }
+
+    function cartel(title, text) {
+        const Toast = Swal.fire({
+            title: title,
+            text: text,
+            icon: 'info',
+            showConfirmButton: true,
+            timer: 5000,
+            timerProgressBar: true,
+        })
+        ;
+    }
 });
